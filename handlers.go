f5e1c9b8b27e7c4cfd5596b87e196065f6ff764b
0x0ff <0x0ff@onsats.org>
2022-12-28 23:54:12 +0100

    log ip and origin

--- handlers.go
+++ handlers.go
@@ -59,6 +59,7 @@ func (s *Server) handleWebsocket(w http.ResponseWriter, r *http.Request) {
 			s.clientsMu.Lock()
 			if _, ok := s.clients[conn]; ok {
 				fmt.Println("Close connection in defer reder func")
+				s.Log.Errorf("ip=%s, origin=", err, conn.RemoteAddr().String(), r.Header["Origin"])
 				conn.Close()
 				delete(s.clients, conn)
 
@@ -247,7 +248,7 @@ func (s *Server) handleWebsocket(w http.ResponseWriter, r *http.Request) {
 				fmt.Println("Number of clients", len(s.clients))
 				err := ws.WriteMessage(websocket.PingMessage, nil)
 				if err != nil {
-					s.Log.Errorf("error writing ping: %v; closing websocket", err)
+					s.Log.Errorf("error writing ping: %v; closing websocket; ip=%s, origin=", err, conn.RemoteAddr(), r.Header["Origin"])
 					return
 				}
 				GetListenerCount()
