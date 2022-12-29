a8638e232c206fd0141b80abe499f4dd7ed9cde7
0x0ff <0x0ff@onsats.org>
2022-12-29 09:57:23 +0100

    test

--- start.go
+++ start.go
@@ -128,7 +128,7 @@ func (s *Server) Start() error {
 func (s *Server) startListener(ln net.Listener) error {
 	// init the relay
 	if err := s.relay.Init(); err != nil {
-		return fmt.Errorf("relay init: %w", err)
+		return fmt.Errorf("relay ini1t: %w", err)
 	}
 	if err := s.relay.Storage().Init(); err != nil {
 		return fmt.Errorf("storage init: %w", err)
