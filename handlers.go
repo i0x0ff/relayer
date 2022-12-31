3ec1d8cf9266f321ce2c7f21fd233630b59bb80f
0x0ff <0x0ff@onsats.org>
2022-12-31 09:54:35 +0100

    Revert "handle defer in handleWebsocket differently"
    This reverts commit 449f0ef12b8b869ee287854c0b370a36c63f3630.

--- handlers.go
+++ handlers.go
@@ -50,9 +50,6 @@ func (s *Server) handleWebsocket(w http.ResponseWriter, r *http.Request) {
 	s.clients[conn] = struct{}{}
 	ticker := time.NewTicker(pingPeriod)
 
-	defer ticker.Stop()
-	defer conn.Close()
-
 	ws := &WebSocket{conn: conn}
 
 	connection := &Connection{
@@ -61,28 +58,18 @@ func (s *Server) handleWebsocket(w http.ResponseWriter, r *http.Request) {
 		origin: r.Header.Get("Origin"),
 	}
 
-	defer func() {
-		s.clientsMu.Lock()
-		if _, ok := s.clients[conn]; ok {
-			// conn.Close()
-			delete(s.clients, conn)
-		}
-		s.clientsMu.Unlock()
-		removeListener(connection)
-	}()
-
 	// reader
 	go func() {
-		// defer func() {
-		// 	// ticker.Stop()
-		// 	s.clientsMu.Lock()
-		// 	if _, ok := s.clients[conn]; ok {
-		// 		// conn.Close()
-		// 		delete(s.clients, conn)
-		// 		removeListener(connection)
-		// 	}
-		// 	s.clientsMu.Unlock()
-		// }()
+		defer func() {
+			ticker.Stop()
+			s.clientsMu.Lock()
+			if _, ok := s.clients[conn]; ok {
+				conn.Close()
+				delete(s.clients, conn)
+				removeListener(connection)
+			}
+			s.clientsMu.Unlock()
+		}()
 
 		conn.SetReadLimit(maxMessageSize)
 		conn.SetReadDeadline(time.Now().Add(pongWait))
@@ -250,12 +237,12 @@ func (s *Server) handleWebsocket(w http.ResponseWriter, r *http.Request) {
 
 	// writer
 	go func() {
-		// defer func() {
-		// 	// ticker.Stop()
-		// 	// conn.Close()
-		// 	delete(s.clients, conn)
-		// 	removeListener(connection)
-		// }()
+		defer func() {
+			ticker.Stop()
+			conn.Close()
+			delete(s.clients, conn)
+			removeListener(connection)
+		}()
 
 		for {
 			select {
